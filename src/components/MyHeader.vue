4ac66dd513eaae901780497553662c909e508d32
Giuseppe0197 <faro.g@hotmail.it>
2021-12-13 13:53:48 +0100

    add header section

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,16 +1,150 @@
 <template>
-  <div >
+  <header>
+      <div>
+          <MyHeaderSub
+          v-for="list, i in headerList"
+          :key="i"
+          :details="list" />
+      </div>
 
-  </div>
+      <div class="team">
+
+        <img class="arrowteam arrowteamleft" src="../assets/img/svg-6.svg">
+
+        <div class="ourteam">
+
+            <h2>Our Team <span class="dot">.</span></h2>
+
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque, corporis? Assumenda porro, amet voluptatibus, consequuntur dolorum.</p>
+
+            <button class="read">read more <span class="borderread"></span><img src="../assets/img/svg-6.svg"></button>
+            <button class="purchase">purchase <span class="borderpur"></span><img src="../assets/img/svg-6.svg"></button>
+        </div>
+
+        <img class="arrowteam arrowteamrigth" src="../assets/img/svg-6.svg">
+    </div>
+
+  </header>
 </template>
 
 <script>
+import MyHeaderSub from '@/components/MyHeaderSub.vue'
+
 export default {
-  name: 'MyHeader'
+  name: 'MyHeader',
+  components: {
+      MyHeaderSub
+  },
+  data() {
+
+      return {
+          headerList: [
+              {
+                  'image': 'logo-img-01.png',
+                  'imageArrow': 'svg-6.svg',
+                  'home': 'homes',
+                  'page': 'pages',
+                  'blog': 'blog',
+                  'shop': 'shop',
+                  'event': 'events',
+                  'element': 'elements',
+
+              }
+          ]
+      }
+
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+
+    header {
+        background-image: url("../assets/img/rev-slider-main-home-img-03.jpg");
+        background-size: cover;
+        background-repeat: no-repeat;
+        background-position: center;
+        height: 650px;
+
+        img {
+            filter: invert(1);
+            
+        }
+
+        .arrowteam {
+            filter: invert(1);
+        }
+
+        .team {
+            width: 90%;
+            margin: 170px auto 0;
+        }
+
+        .arrowteamleft {
+            transform: rotate(180deg);
+            float: left;
+        }
+
+        .ourteam {
+            float: left;
+            margin-left: 150px;
+            width: 35%;
+
+            .dot {
+                color: #ff4612;
+            }
+
+            h2 {
+                font-size: 50px;
+            }
+
+            p {
+                color: #ccc;
+            }
+            
+            button {
+                border: none;
+                padding: 10px;
+                text-transform: uppercase;
+                font-size: 12px;
+
+                img {
+                    vertical-align: middle;
+                    margin-left: 10px;
+                }
+
+                .borderread {
+                    border-left: 2px solid black;
+                    margin-left: 15px;
+                    
+                }
+
+                .borderpur {
+                    border-left: 2px solid white;
+                    margin-left: 15px;
+                }
+            }
+
+            .read {
+                background-color: white;
+            }
+
+            .purchase {
+                background-color: #ff4612;
+                margin-left: 30px;
+                color: white;
+
+                img {
+                    filter: invert(0);
+                }
+            }
+        }
+
+        .arrowteamrigth {
+            float: right;
+        }
+
+    }
 
 </style>
---
+++ src/components/MyHeaderSub.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="subheader">
+
+    <div class="headerlinks">
+
+        <div class="logo">
+            <img :src="require(`../assets/img/${details.image}`)">
+        </div>
+
+        <div class="menuEl">
+            <ul>
+                <li><img class="arrowImage" :src="require(`../assets/img/${details.imageArrow}`)"></li>
+                <li class="home">{{details.home}}</li>
+                <li>{{details.page}}</li>
+                <li>{{details.blog}}</li>
+                <li>{{details.shop}}</li>
+                <li><i class="fas fa-search"></i></li>
+                <li><i class="fas fa-bars"></i></li>
+            </ul>
+        </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyHeaderSub',
+  props: {
+      details: Object
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+.headerlinks {
+    display: flex;
+    justify-content: space-between;
+    width: 90%;
+    margin: 0 auto;
+    height: 100px;
+}
+
+.logo img {
+    width: 50%;
+    margin-top: 20px;
+}
+
+ul li{
+    display: inline-block;
+    margin-right: 20px;
+    margin-top: 20px;
+    vertical-align: middle;
+    text-transform: uppercase;
+    font-weight: bold;
+    font-size: 12px;
+}
+
+.home {
+    color: #ff4612;
+}
+
+</style>
